371b2f3f9e4a949a77d2717e332cb6c8d48a144b
Nikhil Nayak <nikhil.nixel@gmail.com>
2020-09-10 21:13:12 +0530

    Switched: InMemoryAuth -> jdbcAuth

--- backend/src/main/java/com/nikhilcodes/curiousbackend/service/SecurityService.java
+++ backend/src/main/java/com/nikhilcodes/curiousbackend/service/SecurityService.java
@@ -13,9 +13,39 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityService extends WebSecurityConfigurerAdapter {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SecurityService(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+//        String hash1 = "$2a$10$0gr7INy2Vh9ElLle3mExy.Z1wKpkkwbRme.OmSossvYn1fr4lPUhW";
+//        String hash2 = "$2a$10$YVtlaS89K0KVenSQOwQsm.ZTkZ4lLUsSw99AyhLnOYkP3TgqS9Y9K";
+//        auth.inMemoryAuthentication().passwordEncoder(passwordEncoder())
+//                .withUser("admin@nixel.com")
+//                .password(hash1)
+//                .roles("USER")
+//                .and()
+//                .withUser("nikhil.nixel@gmail.com")
+//                .password(hash2)
+//                .roles("ADMIN");
+
+        auth.jdbcAuthentication().passwordEncoder(passwordEncoder())
+                .dataSource(dataSource)
+                .usersByUsernameQuery("SELECT email, password, enabled FROM users_db WHERE email=?") // NOTE: We are considering email as username from login-form
+                .authoritiesByUsernameQuery("SELECT email, role FROM users_db WHERE email=?");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
@@ -23,19 +53,6 @@ public class SecurityService extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .anyRequest()
                 .authenticated();
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth)
-            throws Exception {
-        auth.inMemoryAuthentication().passwordEncoder(passwordEncoder())
-                .withUser("admin@nixel.com")
-                .password(passwordEncoder().encode("password"))
-                .roles("USER")
-                .and()
-                .withUser("nikhil.nixel@gmail.com")
-                .password(passwordEncoder().encode("password"))
-                .roles("ADMIN");
     }
 
     public PasswordEncoder passwordEncoder() {
